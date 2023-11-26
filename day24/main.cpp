86d1b1becde4c85b97a082b42a8d90e9976dbe25
David Jones <djonesuk@gmail.com>
2023-11-26 02:39:27 +0000

    2015 - Day 24

---
+++ day24/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+
+void partOne() {
+    std::ifstream input("..\\day24\\input.txt");
+
+    std::cout << "Day 23: Part One = " << 0 << std::endl;
+}
+
+void partTwo() {
+
+}
+
+int main() {
+    partOne();
+    partTwo();
+}
