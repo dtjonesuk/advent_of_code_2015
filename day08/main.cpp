22b607b1b1f66e5085b881dd02ddba2ab04eba7e
David Jones <djonesuk@gmail.com>
2023-12-03 13:05:59 +0000

    Refactor Day 08

--- day08/main.cpp
+++ day08/main.cpp
@@ -1,118 +1,134 @@
-//
-// Created by David on 24/10/2023.
-//
-#include <iostream>
-#include <string>
-#include <iterator>
-#include <fstream>
-#include <cassert>
+#include "../advent.h"
 
-// count the unused chars in a quote-string
-int countChars(const std::string &str) {
-    int len = str.size();
-    int unused = 0;
+namespace day08 {
+/// Change this to the current day
+    static const std::string day = "08";
+    static const std::string inputFilename = "..\\day" + day + "\\input.txt";
+//    static const std::string testFilename = "..\\day" + day + "\\test.txt";
 
-    for (auto it = str.begin(); it != str.end(); ++it) {
-        if (*it == '"') {
-            ++unused;
-        } else if (*it == '\\') {
-            ++it;
-            switch (*it) {
-                case '\\':
-                case '"':
-                    ++unused;
-                    break;
-                case 'x':
-                    std::string hex = {*++it, *++it};
-                    unused += 3;
-                    break;
+    struct PuzzleInput {
+        std::vector<std::string> lines;
+
+        explicit PuzzleInput(const std::string &filename) {
+            std::ifstream istream(filename);
+            std::string line;
+
+            while (std::getline(istream, line)) {
+                // do something
+                lines.emplace_back(std::move(line));
             }
         }
-    }
 
-    return unused;
-}
+    };
 
-std::string encodeString(const std::string &str) {
-    std::string output = "\"";
-    for (auto it = str.begin(); it != str.end(); ++it) {
-        if (*it == '"') {
-            output += "\\\"";
-        } else {
-            if (*it == '\\') {
+    // count the unused chars in a quote-string
+    int countChars(const std::string &str) {
+        int unused = 0;
+
+        for (auto it = str.begin(); it != str.end(); ++it) {
+            if (*it == '"') {
+                ++unused;
+            } else if (*it == '\\') {
                 ++it;
                 switch (*it) {
                     case '\\':
-                        output += "\\\\\\\\";
-                        break;
-                    case '\"':
-                        output += "\\\\\\\"";
+                    case '"':
+                        ++unused;
                         break;
                     case 'x':
                         std::string hex = {*++it, *++it};
-                        output += "\\\\x" + hex;
+                        unused += 3;
                         break;
                 }
-
-            } else {
-                output += *it;
             }
         }
-    }
-    output += "\"";
-    return output;
-}
 
-void test(const std::string &str) {
-    std::cout << str << " : " << encodeString(str) << " " << encodeString(str).size() << " " << str.size() << std::endl;
-}
-
-void testFunc() {
-    assert(countChars("\"\"") == 2);
-    assert(countChars("\"abc\"") == 2);
-    assert(countChars("\"aaa\\\"aaa\"") == 3);
-    assert(countChars("\"\\x27\"") == 5);
-
-//    test("\"\"");
-//    test("\"abc\"");
-//    test("\"aaa\\\"aaa\"");
-//    test("\"\\x27\"");
-
-
-}
-
-void partOne() {
-    std::ifstream input("..\\day08\\input.txt");
-
-
-    std::string line;
-    int total = 0;
-
-    while (std::getline(input, line)) {
-        total += countChars(line);
+        return unused;
     }
 
-    std::cout << "Part One : " << total << std::endl;
-}
+    std::string encodeString(const std::string &str) {
+        std::string output = "\"";
+        for (auto it = str.begin(); it != str.end(); ++it) {
+            if (*it == '"') {
+                output += "\\\"";
+            } else {
+                if (*it == '\\') {
+                    ++it;
+                    switch (*it) {
+                        case '\\':
+                            output += "\\\\\\\\";
+                            break;
+                        case '\"':
+                            output += "\\\\\\\"";
+                            break;
+                        case 'x':
+                            std::string hex = {*++it, *++it};
+                            output += "\\\\x" + hex;
+                            break;
+                    }
 
-void partTwo() {
-    std::ifstream input("..\\day08\\input.txt");
-
-
-    std::string line;
-    int total = 0;
-
-    while (std::getline(input, line)) {
-        std::string encoded = encodeString(line);
-        total += encoded.size() - line.size();
+                } else {
+                    output += *it;
+                }
+            }
+        }
+        output += "\"";
+        return output;
     }
 
-    std::cout << "Part Two : " << total << std::endl;
+/// Unit Test
+    void test() {
+        assert(countChars("\"\"") == 2);
+        assert(countChars("\"abc\"") == 2);
+        assert(countChars("\"aaa\\\"aaa\"") == 3);
+        assert(countChars("\"\\x27\"") == 5);
+    }
+
+/// Part One Solution
+    int partOne() {
+        PuzzleInput input(inputFilename);
+
+        int total = std::transform_reduce(input.lines.begin(), input.lines.end(), 0, std::plus{}, countChars);
+
+        return total;
+    }
+
+
+/// Part Two Solution
+    int partTwo() {
+        PuzzleInput input(inputFilename);
+
+        int total = std::transform_reduce(input.lines.begin(), input.lines.end(), 0, std::plus{}, [](const std::string& line) {
+            std::string encoded = encodeString(line);
+            return static_cast<int>(encoded.size() - line.size());
+        });
+
+        return total;
+
+    }
 }
 
 int main() {
-    testFunc();
+    using namespace day08;
+    test();
 
-    partOne();
-    partTwo();
+    {
+        std::cout << std::fixed << std::setprecision(3);
+        auto [seconds, result] = advent::eval<int>(&partOne);
+
+        std::cout << "Day " << day << ": Part One = " << result << "\t\t (completed in " << seconds << "s).\n";
+    }
+    {
+        std::cout << std::fixed << std::setprecision(3);
+        auto [seconds, result] = advent::eval<int>(&partTwo);
+
+        std::cout << "Day " << day << ": Part One = " << result << "\t\t (completed in " << seconds << "s).\n";
+    }
 }
+
+
+
+
+
+
+
