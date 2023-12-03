79f2ee22686cf3ec90d32bd2fdf2cf6e16d0c0f5
David Jones <djonesuk@gmail.com>
2023-12-03 01:03:41 +0000

    Add boilerplate template and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2015
+
+This is my attempt at Advent of Code 2015.
+
+My language of choice is C++ and I have developed a minimal boilerplate template to facilitate setting up each day. (
+See `main.cpp` and `advent.h` for details).
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,68 @@
-#include <iostream>
+#include "../advent.h"
+
+namespace day## {
+    /// Change this to the current day
+    static const std::string day = "##";
+    static const std::string inputFilename = "..\\day" + day + "\\input.txt";
+    static const std::string testFilename = "..\\day" + day + "\\test.txt";
+
+    struct PuzzleInput {
+        std::vector<std::string> lines;
+
+        explicit PuzzleInput(const std::string &filename) {
+            std::ifstream istream(filename);
+            std::string line;
+
+            while (std::getline(istream, line)) {
+                // do something
+                lines.emplace_back(std::move(line));
+            }
+        }
+
+    };
+
+    /// Unit Test
+    void test() {
+        PuzzleInput input(testFilename);
+        assert(true);
+    }
+
+    /// Part One Solution
+    int partOne() {
+        using namespace std::views;
+
+        PuzzleInput input(inputFilename);
+
+        return 0;
+    }
+
+
+    /// Part Two Solution
+    int partTwo() {
+        using namespace std::views;
+
+        PuzzleInput input(inputFilename);
+
+        return 0;
+    }
+
+    void run() {
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    using namespace day##;
+    test();
+
+    {
+        std::cout << std::fixed << std::setprecision(3);
+        auto [seconds, result] = advent::eval<int>(&partOne);
+
+        std::cout << "Day " << day << ": Part One = " << result << "\t\t (completed in " << seconds << "s).\n";
+    }
+    {
+        std::cout << std::fixed << std::setprecision(3);
+        auto [seconds, result] = advent::eval<int>(&partTwo);
+
+        std::cout << "Day " << day << ": Part One = " << result << "\t\t (completed in " << seconds << "s).\n";
+    }
 }
